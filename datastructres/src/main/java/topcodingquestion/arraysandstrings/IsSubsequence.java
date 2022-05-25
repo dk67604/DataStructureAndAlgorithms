7adb3e7c1c532bc37e40a0f5d4645a30ccc1d1aa
Dharamendra Kumar <dharamendra.kumar@outlook.com>
2022-05-24 20:37:18 -0700

    Update IsSubsequence.java

--- datastructres/src/main/java/topcodingquestion/arraysandstrings/IsSubsequence.java
+++ datastructres/src/main/java/topcodingquestion/arraysandstrings/IsSubsequence.java
@@ -1,4 +1,16 @@
 package main.java.topcodingquestion.arraysandstrings;
 
 public class IsSubsequence {
+  public boolean isSubsequence(String s, String t) {
+       int left = s.length();
+       int right = t.length();
+       int pLeft = 0, pRight = 0;
+       while(pLeft<left && pRight < right){
+           if(s.charAt(pLeft) == t.charAt(pRight)){
+               pLeft++;
+           }
+           pRight++;
+       }
+        return pLeft == left;
+    }
 }
