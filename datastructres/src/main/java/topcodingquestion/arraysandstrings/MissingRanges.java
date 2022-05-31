18de02dde877795dd7068e0ea51218375e1e4c25
Dharamendra Kumar <dharamendra.kumar@outlook.com>
2022-05-31 11:49:41 -0700

    Update MissingRanges.java

--- datastructres/src/main/java/topcodingquestion/arraysandstrings/MissingRanges.java
+++ datastructres/src/main/java/topcodingquestion/arraysandstrings/MissingRanges.java
@@ -2,7 +2,21 @@ package main.java.topcodingquestion.arraysandstrings;
 
 import java.util.ArrayList;
 import java.util.List;
+/**
+ou are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are in the inclusive range.
 
+A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+
+Return the smallest sorted list of ranges that cover every missing number exactly. That is, no element of nums is in any of the ranges, and each missing number is in one of the ranges.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+
+
+*/
 public class MissingRanges {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         int prev = Integer.MIN_VALUE;
