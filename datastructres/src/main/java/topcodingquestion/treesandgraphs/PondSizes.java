e0abc72fd34fde25bc5cf0a54747dc5abd60e42f
Dharamendra Kumar <dharamendra.kumar@outlook.com>
2022-06-11 00:15:27 -0700

    Update PondSizes.java

--- datastructres/src/main/java/topcodingquestion/treesandgraphs/PondSizes.java
+++ datastructres/src/main/java/topcodingquestion/treesandgraphs/PondSizes.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 //CTCI Moderate 16.19
+/**
+ * You have an integer matrix representing a plot of land, where the value at that location
+ represents the height above sea level. A value of zero indicates water. A pond is a region of water
+ connected vertically, horizontally, or diagonally. The size of the pond is the total number of
+ connected water cells. Write a method to compute the sizes of all ponds in the matrix.
+ EXAMPLE
+ Input:
+ 0 2 1 0
+ 1 1 0 1
+ 0 1 0 1
+ 0 1 0 1
+ Output: 2, 4, 1 (in any order)
+ */
 public class PondSizes {
     public static List<Integer> computePondSizes(int[][] land) {
         List<Integer> pondSizes = new ArrayList<>();
